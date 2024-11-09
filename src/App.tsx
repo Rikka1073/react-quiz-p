306e2131da84aad621432b044656914ac4a5fd5a
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2024-11-09 20:55:39 +0900

    クイズ作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,47 @@
+import { useState } from "react";
 import "./App.css";
 
+const QUESTION = "Reactはどのようなものですか？";
+const ANSWERS = ["データベース", "フレームワーク", "ライブラリ", "言語"];
+const CORRECT_ANSWER = "ライブラリ";
+
 function App() {
+  const [userSelect, setUserSelect] = useState("");
+  const [result, setResult] = useState("");
+  const onclickUser = (answer: string) => {
+    setUserSelect(answer);
+    if (answer === CORRECT_ANSWER) {
+      setResult("正解です");
+    } else {
+      setResult("残念、不正解です");
+    }
+  };
+
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div className="mx-auto w-5/6 bg-orange-100 h-screen">
+        <div className="text-center pt-10">
+          <p className="text-2xl">{QUESTION}</p>
+        </div>
+        <div className="text-center mt-2 flex gap-3 justify-center">
+          {ANSWERS.map((answer, index) => {
+            return (
+              <div
+                key={index}
+                className={`border - 2 border-green-200 rounded-lg px-3 py-2 ${
+                  answer === userSelect && "bg-black text-white"
+                }`}
+              >
+                <div onClick={() => onclickUser(answer)}>{answer}</div>
+              </div>
+            );
+          })}
+        </div>
+        <div className="text-center pt-10">
+          <p className="text-2xl">結果表示</p>
+          <p className="text-xl text-red-600">{result}</p>
+        </div>
+      </div>
     </>
   );
 }
